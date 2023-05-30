10a7c3395c65baa5ff2e9d8d91ff0df4bf3eb495
Louie T <tselouie@gmail.com>
2023-05-30 16:29:59 -0400

    added ipfs.io

--- next.config.js
+++ next.config.js
@@ -2,14 +2,15 @@
 const nextConfig = {
   images: {
     dangerouslyAllowSVG: true,
-    domains:[
-      'static.nftgo.io', 
-      'images.wrappedpunks.com',
-      'api.mpunks.org',
-      'metadata.degods.com',
-      '*.alchemy.com',
-      'res.cloudinary.com',
-      '*.ipfs.io'
+    domains: [
+      "static.nftgo.io",
+      "images.wrappedpunks.com",
+      "api.mpunks.org",
+      "metadata.degods.com",
+      "*.alchemy.com",
+      "res.cloudinary.com",
+      "*.ipfs.io",
+      "ipfs.io",
     ],
     remotePatterns: [
       {

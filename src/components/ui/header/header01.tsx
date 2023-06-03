313e69b0859ef65423c5424d6f323494069da938
khuephamy <khuephumy19@gmail.com>
2023-06-03 08:36:08 -0500

    clear input for second autocomplete modal

--- src/components/ui/header/header01.tsx
+++ src/components/ui/header/header01.tsx
@@ -1111,6 +1111,24 @@ export default function Header01() {
               <path d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z" />
             </svg>
           </span>
+
+          {enteredWord.length == 0 ? (
+              <span></span>
+            ) : (
+              <span className="absolute right-0 top-0 flex h-full w-12 items-center justify-center rounded-2xl" onClick={clearInput}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  width={26}
+                  height={26}
+                  className="fill-jacarta-500 h-4 w-4 dark:fill-white cursor-pointer"
+
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+                </svg>
+              </span>
+            )}
         </form>
         {/* End search form mobile menu  */}
 

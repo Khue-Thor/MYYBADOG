2f1ed941e4411eacc7deb1b6077b45c8af75f93e
Louie T <tselouie@gmail.com>
2023-06-08 19:31:43 -0400

    prettier

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,14 +5,11 @@ import {
   import NextAuth from "next-auth";
   import type { NextAuthOptions } from 'next-auth'
   
-  export const AuthOptions:NextAuthOptions = {
-       session:{strategy:"jwt"},
+  export const authOptions:NextAuthOptions = {
        providers: [
-   
       // Add the thirdweb auth provider to the providers configuration
       ThirdwebAuthProvider({
         domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN || "",
-
       }),
 
     ],
@@ -22,9 +19,9 @@ import {
         const sessionWithAddress = authSession({ session, token });
         return sessionWithAddress;
       }  
-    },
+    }
   }
   
-  const handler= NextAuth(AuthOptions);
+  const handler= NextAuth(authOptions);
 
   export {handler as GET, handler as POST}

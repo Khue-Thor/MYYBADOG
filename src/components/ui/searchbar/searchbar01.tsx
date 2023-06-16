2d3c5a3f99a7f1fe8a3fc05c2fcea73481141197
khuephamy <khuephumy19@gmail.com>
2023-06-16 15:22:06 -0500

    feat: no records

--- src/components/ui/searchbar/searchbar01.tsx
+++ src/components/ui/searchbar/searchbar01.tsx
@@ -60,23 +60,39 @@ const SearchBar01 = () => {
 
 
 
+  // const handleFilter = (e: any) => {
+  //   const searchWord = e.target.value;
+  //   setEnteredWord(searchWord);
+  //   const newFilter = filteredData.filter((value: any) => {
+  //     // Check if collectionName is not null before applying toLowerCase()
+  //     if (value.openSeaMetadata.collectionName !== null) {
+  //       return value.openSeaMetadata.collectionName.toLowerCase().includes(searchWord.toLowerCase());
+  //     }
+  //     return false; // Skip the current value if collectionName is null
+  //   });
+
+  //   if (searchWord === "") {
+  //     setCollectionsData([])
+  //   } else {
+  //     setCollectionsData(newFilter);
+  //   }
+  // };
+
   const handleFilter = (e: any) => {
-    const searchWord = e.target.value;
+    const searchWord = e.target.value.toLowerCase(); // Convert search word to lowercase
     setEnteredWord(searchWord);
+
     const newFilter = filteredData.filter((value: any) => {
-      // Check if collectionName is not null before applying toLowerCase()
       if (value.openSeaMetadata.collectionName !== null) {
-        return value.openSeaMetadata.collectionName.toLowerCase().includes(searchWord.toLowerCase());
+        const collectionName = value.openSeaMetadata.collectionName.toLowerCase(); // Convert collection name to lowercase
+        return collectionName.includes(searchWord);
       }
-      return false; // Skip the current value if collectionName is null
+      return false;
     });
 
-    if (searchWord === "") {
-      setCollectionsData([])
-    } else {
-      setCollectionsData(newFilter);
-    }
+    setCollectionsData(newFilter); // Always set the filtered data, even if searchWord is empty
   };
+
 
   const clearInput = () => {
     setCollectionsData([]);
@@ -98,12 +114,12 @@ const SearchBar01 = () => {
       />
 
 
-      {enteredWord.length >= 3 && collectionsData.length !== 0 && (
-        <div className="overflow-scroll dark:bg-jacarta-700  bg-white text-black absolute z-10 drop-shadow-lg left-[0px] top-[55px] pt-3 w-full pb-[20px]  rounded-2xl flex flex-col gap-1 pr-[10px] pl-[10px]">
+      {enteredWord.length >= 3 && collectionsData.length !== 0 ? (
+        <div className="overflow-scroll dark:bg-jacarta-700 bg-white text-black absolute z-10 drop-shadow-lg left-[0px] top-[55px] pt-3 w-full pb-[20px] rounded-2xl flex flex-col gap-1 pr-[10px] pl-[10px]">
           <span className='font-bold text-sm text-gray-600 p-3'>COLLECTIONS</span>
           {collectionsData.slice(0, 5).map((value: any) => {
             return (
-              <div key={value.address} className="p-1 dark:hover:bg-jacarta-600  hover:bg-gray-400 hover:rounded-xl flex justify-between pr-3 pl-3 pt-2 pb-2 cursor-pointer">
+              <div key={value.address} className="p-1 dark:hover:bg-jacarta-600 hover:bg-gray-400 hover:rounded-xl flex justify-between pr-3 pl-3 pt-2 pb-2 cursor-pointer">
                 <div className="flex gap-3 items-top">
                   <img src={value.openSeaMetadata.imageUrl} alt="Image" className="rounded-lg w-9 h-9" />
                   <div className="flex flex-col">
@@ -117,7 +133,11 @@ const SearchBar01 = () => {
           })}
           <span className='font-bold text-sm text-gray-600 p-3'>ACCOUNTS</span>
         </div>
-      )}
+      ) : enteredWord.length >= 3 ? (
+        <div className="overflow-scroll dark:bg-jacarta-700 bg-white text-black absolute z-10 drop-shadow-lg left-[0px] top-[55px] pt-3 w-full pb-[20px] rounded-2xl flex flex-col gap-1 pr-[10px] pl-[10px]">
+          <p className="font-bold text-sm text-gray-600 p-3">No records</p>
+        </div>
+      ) : null}
 
 
 

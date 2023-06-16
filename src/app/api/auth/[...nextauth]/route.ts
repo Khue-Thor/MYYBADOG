79af00742b8559c6c2e0948c4ce24f5a2d7b5557
Louie Tse <tselouie@gmail.com>
2023-06-16 10:14:24 -0400

    workaround for omit bug

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,23 +4,7 @@ import {
   } from "@thirdweb-dev/auth/next-auth";
   import NextAuth from "next-auth";
   import type { NextAuthOptions } from 'next-auth'
-  
-  export const authOptions:NextAuthOptions = {
-       providers: [
-      // Add the thirdweb auth provider to the providers configuration
-      ThirdwebAuthProvider({
-        domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN || "",
-      }),
-
-    ],
-    secret:process.env.NEXTAUTH_SECRET,
-    callbacks: {
-      async session({ session, user, token }) {
-        const sessionWithAddress = authSession({ session, token });
-        return sessionWithAddress;
-      }  
-    }
-  }
+  import { authOptions } from "@/lib/auth";
   
   const handler= NextAuth(authOptions);
 

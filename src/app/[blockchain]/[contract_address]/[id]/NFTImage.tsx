95fc0a57473a5ef6971172855e73c9623581874b
Roman Tokar <rtokar.appwell@ancestry.com>
2023-06-20 23:01:19 +0300

    Converting img tag to Image component for the nft images

--- src/app/[blockchain]/[contract_address]/[id]/NFTImage.tsx
+++ src/app/[blockchain]/[contract_address]/[id]/NFTImage.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState } from "react";
+import defaultNFTImage from "@/public/images/case-studies/case_study_4.jpg";
+import Image from "next/image";
 
 type Props = {
   image: string;
@@ -10,27 +12,31 @@ const NFTImage: React.FC<Props> = ({ image, name }) => {
   const [imageModal, setImageModal] = useState(false);
 
   return (
-    <figure className="mb-8 md:w-2/5 md:flex-shrink-0 md:flex-grow-0 md:basis-auto lg:w-1/2 w-full">
+    <figure className="mb-8 md:mb-0 md:w-2/5 md:flex-shrink-0 md:flex-grow-0 md:basis-auto lg:w-1/2 w-full h-96 md:h-auto">
       <button
-        className="w-full"
+        className="w-full relative h-full"
         onClick={() => {
           setImageModal(true);
         }}
       >
-        <img
-          src={image || "/images/avatars/avatar_1"}
+        <Image
+          src={image || defaultNFTImage}
           alt={`${name} NFT image`}
-          className="rounded-2xl cursor-pointer  w-full"
+          fill
+          className="object-cover rounded-2xl"
+          sizes="(max-width: 768px) 100vw, (max-width: 1025px) 40vw, 50vw"
         />
       </button>
 
       {/* <!-- Modal --> */}
       <div className={imageModal ? "modal fade show block" : "modal fade"}>
-        <div className="modal-dialog !my-0 flex h-full max-w-4xl items-center justify-center">
-          <img
-            src={image || "/images/avatars/avatar_1"}
+        <div className="modal-dialog !my-0 flex h-full max-w-4xl items-center justify-center rounded-2xl w-full relative">
+          <Image
+            src={image || defaultNFTImage}
             alt={`${name} NFT image`}
-            className="h-full rounded-2xl"
+            fill
+            sizes={"100vw"}
+            className="object-cover rounded-2xl"
           />
         </div>
 

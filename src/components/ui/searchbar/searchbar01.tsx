25352f094015526571ce09d807a1c8ec0b6255f4
thiago <thidurante@hotmail.com>
2023-06-26 23:02:45 -0300

    close handle on searchbar01

--- src/components/ui/searchbar/searchbar01.tsx
+++ src/components/ui/searchbar/searchbar01.tsx
@@ -238,6 +238,19 @@ const SearchBar01 = () => {
           </span>
         </div>
       )}
+      <span className="absolute right-0 top-0 flex h-full w-12 items-center justify-center rounded-2xl" onClick={clearInput}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          width={26}
+          height={26}
+          className="fill-jacarta-500 h-4 w-4 dark:fill-white cursor-pointer"
+
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+        </svg>
+      </span>
     </form>
   );
 };

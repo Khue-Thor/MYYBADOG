88ba47dc60b579a52f72f8afc9d857f996ad3cb4
khuephamy <khuephumy19@gmail.com>
2023-07-10 14:09:56 -0500

    fix: most voted button toggle

--- src/app/request/page.tsx
+++ src/app/request/page.tsx
@@ -10,8 +10,12 @@ const page = () => {
   const [mostVotedModalOpen, setMostVotedModalOpen] = useState(false);
   const [requestModalOpen, setRequestModalOpen] = useState(false);
 
+  // const handleToggleMostVotedModal = () => {
+  //   setMostVotedModalOpen((prevOpen) => !prevOpen);
+  // };
+
   const handleToggleMostVotedModal = () => {
-    setMostVotedModalOpen((prevOpen) => !prevOpen);
+    setMostVotedModalOpen(!mostVotedModalOpen);
   };
   return (
     <div className='flex justify-center pt-20'>
@@ -30,11 +34,25 @@ const page = () => {
         </div>
         <div className='bg-accent-dark pt-4 pb-4 pl-10 pr-10 flex justify-between border-jacarta-400 border-t-[1px] border-b-[1px]'>
           <div className='flex gap-5'>
-            <div className='hover:bg-gray-700 w-fit p-1 rounded-sm relative'>
-              <button className='text-sm' onClick={handleToggleMostVotedModal}>Most voted</button>
-              {mostVotedModalOpen && (
-                <MostVotedModal />
-              )}
+            <div className='hover:bg-gray-700 w-fit pt-1 pb-1 pl-3 pr-3 rounded-sm relative flex flex-row items-center gap-2'>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  width={26}
+                  height={26}
+                  className={`fill-jacarta-500 h-[20px] w-[20px] dark:fill-jacarta-600 cursor-pointer ${mostVotedModalOpen ? 'transform rotate-90' : ''}`}
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M4.41 5.41L5.83 4 11.83 10 5.83 16 4.41 14.59 8.83 10 4.41 5.41z" />
+                </svg>
+              </span>
+
+              <button className='text-sm' onClick={handleToggleMostVotedModal}>
+                Most voted
+              </button>
+
+              {mostVotedModalOpen && <MostVotedModal />}
             </div>
 
             <div className='hover:bg-gray-700 w-fit p-1 rounded-sm'>
--- src/components/ui/request/user-request.tsx
+++ src/components/ui/request/user-request.tsx
@@ -31,18 +31,42 @@ const UserRequest = ({ title, details }) => {
         <p className='truncate'>{title}</p>
         <p className='text-sm text-gray-600'>{details}</p>
         <div>
-            <p className='text-xs'>Status</p>
+          <p className='text-xs'>Status</p>
         </div>
       </div>
-      <div className='flex flex-col gap-3'>
+      <div className='flex flex-col gap-2'>
         <div
-          className={`dark:bg-gray-700 pt-1 pb-1 pl-3 pr-3 rounded-md cursor-pointer ${userVoted ? 'bg-blue-500' : ''
+          className={`dark:bg-gray-700 dark:hover:bg-gray-600 pt-1 pb-1 pl-3 pr-3 flex items-center gap-2 rounded-md cursor-pointer ${userVoted ? 'bg-blue-500' : ''
             }`}
           onClick={handleLikeClick}
         >
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              width={26}
+              height={26}
+              className="fill-jacarta-500 h-[20px] w-[20px] dark:fill-jacarta-600 cursor-pointer"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M10 6l5 5H5l5-5z" />
+            </svg>
+          </span>
           <span className='text-xs'>{likeVotes}</span>
         </div>
-        <div className='pt-1 pb-1 pl-3 pr-3 cursor-pointer' onClick={handleDisLikeClick}>
+        <div className='pt-1 pb-1 pl-3 pr-3 cursor-pointer flex flex-row gap-2 items-center' onClick={handleDisLikeClick}>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              width={26}
+              height={26}
+              className="fill-jacarta-500 h-[20px] w-[20px] dark:fill-jacarta-600 cursor-pointer"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M10 14l-5-5h10l-5 5z" />
+            </svg>
+          </span>
           <span className='text-xs'>{dislikeVotes}</span>
         </div>
       </div>
